32b4b17fb3c0012b69c96ec07c6d3f9bd6c9546e
Afzaal Ahmad Zeeshan <justin17862@gmail.com>
2014-09-14 14:16:11 +0500

    Source code
    This commit has the source files required to perform the actions.

---
+++ Source Files/WebSite12/Scripts/Default.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $('#body').css({
+        height: screen.height - 200
+    })
+
+    $('body').keydown(function () {
+        $('#result').text(event.keyCode);
+    });
+});
